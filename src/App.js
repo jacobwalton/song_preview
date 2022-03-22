149a9713e2d158ab9ade1db8b9c693b1800daeee
Jacob Walton <jawalton6616@gmail.com>
2022-03-22 11:55:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  SpectrumVisualizer,
+  SpectrumVisualizerTheme,
+  WaveformVisualizer,
+  WaveformVisualizerTheme,
+} from "react-audio-visualizers";
+
+import { useEffect, useState } from "react";
+import noTime from "./NO_TIME.mp3";
+
+// let noTime = "./assets/NO_TIME.mp3";
+// let noTime = new Audio("/NO_TIME.mp3");
 
 function App() {
+  const answer = "m0R3T1mE4M3";
+  const [response, setResponse] = useState("");
+  const [codeCracked, setCodeCracked] = useState(false);
+  const handleChange = (e) => {
+    setResponse(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (answer === response) {
+      setCodeCracked(true);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="question">
+        <h3>WHAT'S THE SECRET CODE?</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="password" name="answer" onChange={handleChange} />
+          <input type="submit" id="submit" />
+        </form>
+        {codeCracked && (
+          <div className="visualizer">
+            <WaveformVisualizer
+              audio={noTime}
+              theme={WaveformVisualizerTheme.squaredBars}
+              colors={["#900", "#933"]}
+              iconsColor="#9353"
+              // backgroundColor="white"
+              showMainActionIcon
+              showLoaderIcon
+            />
+            {/* <audio
+              id="audio"
+              src={noTime}
+              autoPlay={true}
+              // controls={true}
+              // controlsList="nodownload noplaybackrate"
+            >
+              Your browser does not support the audio element.
+            </audio> */}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
