b7a19cb5e90e18f718df1bf514df0b8a3dea64ed
Jacob Walton <jawalton6616@gmail.com>
2022-03-24 20:04:27 -0500

    changed visualizer style and added btn

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import "./App.css";
 import {
-  SpectrumVisualizer,
-  SpectrumVisualizerTheme,
   WaveformVisualizer,
   WaveformVisualizerTheme,
 } from "react-audio-visualizers";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import noTime from "./NO_TIME.mp3";
-
-// let noTime = "./assets/NO_TIME.mp3";
-// let noTime = new Audio("/NO_TIME.mp3");
 
 function App() {
   const answer = "m0R3T1mE4M3";
   const [response, setResponse] = useState("");
   const [codeCracked, setCodeCracked] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
   const handleChange = (e) => {
     setResponse(e.target.value);
   };
@@ -23,8 +19,15 @@ function App() {
     e.preventDefault();
     if (answer === response) {
       setCodeCracked(true);
+    } else {
+      setErrorMsg("TRY AGAIN");
     }
   };
+
+  const mainActionRender = ({ play, _ }) => ({
+    id: "mainActionContainer",
+    node: <button onClick={play}>Play</button>,
+  });
   return (
     <div className="App">
       <div className="question">
@@ -34,28 +37,21 @@ function App() {
         </h3>
         <form onSubmit={handleSubmit}>
           <input type="password" name="answer" onChange={handleChange} />
-          <input type="submit" id="submit" />
+          {errorMsg && <p className="error">{errorMsg}</p>}
+          <input type="submit" id="submit" value="SUBMIT" />
         </form>
         {codeCracked && (
           <div className="visualizer">
             <WaveformVisualizer
+              className="visualizer"
               audio={noTime}
-              theme={WaveformVisualizerTheme.squaredBars}
-              colors={["#900", "#933"]}
-              iconsColor="#9353"
-              // backgroundColor="white"
+              theme={WaveformVisualizerTheme.line}
+              colors={["#933"]}
+              iconsColor="#933"
               showMainActionIcon
               showLoaderIcon
+              mainActionRender={mainActionRender}
             />
-            {/* <audio
-              id="audio"
-              src={noTime}
-              autoPlay={true}
-              // controls={true}
-              // controlsList="nodownload noplaybackrate"
-            >
-              Your browser does not support the audio element.
-            </audio> */}
           </div>
         )}
       </div>
